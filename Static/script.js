62c31a3144f9dbbe1aac243a16826e370dac2f9f
sujitha648-web <sujithasujitha648@gmail.com>
2026-06-07 12:46:08 +0530

    Initial commit

---
+++ Static/script.js
@@ -0,0 +1,11 @@
+function shop(){
+alert("Welcome to ANJITHA WEAVES Shopping!");
+}
+
+function sendMessage(){
+alert("Your message sent successfully!");
+}
+
+function buyNow(){
+alert("Thank you...Please contact us to get this products!");
+}
